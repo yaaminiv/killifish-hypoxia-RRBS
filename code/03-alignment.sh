abf23c7cf6014b08ab9e291ade170c076f045e8b
Yaamini Venkataraman <yaaminiv@uw.edu>
2022-03-22 11:05:19 -0400

    call diff scripts

--- code/03-alignment.sh
+++ code/03-alignment.sh
@@ -19,34 +19,19 @@ module load singularity/3.7
 
 echo "Mapping Module"
 
-singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/03-alignment-envfile.txt --bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch /vortexfs1/home/naluru/bat_latest.sif \
-  for f in "${FASTQ[@]}"
-  do 
-    BAT_mapping \
-    -g $GENOME \
-    -q ${TRIMMED}/${f}_1_val_1.fq.gz \
-    -p ${TRIMMED}/${f}_2_val_2.fq.gz \
-    -i $INDICES \
-    -o ${SINGMAPPED}/${f} \
-    -t 16 \
-    -F 2
-done
+#Run BAT_mapping script
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/03-alignment-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch /vortexfs1/home/naluru/bat_latest.sif \
+03-BAT-mapping.sh
 
 echo "Done with mapping"
 
 echo "Statistics Module"
 
-STAT=/vortexfs1/scratch/yaamini.venkataraman/03-mapping/stat #Directory on host system for stat files
-
-for f in "${FASTQ[@]}"
-do
-  singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/03-alignment-envfile.txt --bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch /vortexfs1/home/naluru/bat_latest.sif \
-  BAT_mapping_stat \
-  --bam ${SINGMAPPED}/${f}.bam \
-  --excluded ${SINGMAPPED}/${f}.excluded.bam \
-  --fastq ${TRIMMED}/${f}_1_val_1.fq.gz \
-  > ${STAT}/${f}.stat
-done
+#Run BAT_mapping_stat script
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/03-alignment-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch /vortexfs1/home/naluru/bat_latest.sif \
+03-BAT-mapping-stat.sh
 
 echo "Done with statistics"
 
