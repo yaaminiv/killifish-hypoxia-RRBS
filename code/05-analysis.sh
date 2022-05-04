6fbcb9933351782a720313f9f5f70460fef79051
Yaamini Venkataraman <yaaminiv@uw.edu>
2022-05-04 12:15:31 -0400

    update BAT_summarize for all contrasts

--- code/05-analysis.sh
+++ code/05-analysis.sh
@@ -10,13 +10,6 @@
 #SBATCH --output=yrv_analysis%j.log  								   					  		# Standard output
 #SBATCH --chdir=/vortexfs1/scratch/yaamini.venkataraman/05-analysis  	# Working directory for this script
 
-#Exit script if any command fails
-set -e
-
-#Make scripts executable
-chmod +x /vortexfs1/home/yaamini.venkataraman/04-BAT-calling.sh
-chmod +x /vortexfs1/home/yaamini.venkataraman/04-BAT-calling-filtering.sh
-
 #Load the singularity module for BAT
 module load singularity/3.7
 
@@ -48,9 +41,7 @@ echo "Summarize Module"
 
 echo "Population irrespective of oxygen"
 
-#I will summarize methylation information for each population, irrespective of oxygen conditions. This will provide population-specific methylation information. I will exclude OC samples.
-
-mkdir all_pop
+#I will summarize methylation information for each population, irrespective of oxygen conditions. This will provide population-specific methylation information.
 
 #Run BAT_summarize
 #in1: comma-separated bedGraphs from NBH
@@ -63,48 +54,226 @@ singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-env
 --bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
 /vortexfs1/home/naluru/bat_latest.sif \
 BAT_summarize \
---in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-N4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_20-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-N3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-N1_CG.sort.bedgraph \
---in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_5-S1_CG.sort.bedgraph \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-N4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_20-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-N3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N5_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N4_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_5-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_OC-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_OC-S5_CG.sort.bedgraph \
 --groups N,S \
---h1 20-N4,5-N1,5-N2,20-N2,5-N3,20-N1 \
---h2 20-S1,20-S3,20-S4,5-S3,5-S4,5-S2,20-S2,5-S1 \
---out ${ALL_POP} \
---cs ${CHROM_LENGTH} \
--bgbw /yaaminiv/miniconda3/bin/bedGraphToBigWig
+--h1 20-N4,5-N1,5-N2,20-N2,5-N3,20-N1,OC-N5,OC-N1,OC-N2,OC-N4 \
+--h2 20-S1,20-S3,20-S4,5-S3,5-S4,5-S2,20-S2,5-S1,OC-S1,OC-S2,OC-S3,OC-S5 \
+--out /scratch/05-analysis/summarize/all_pop \
+--cs ${CHROM_LENGTH}
 
-echo "Oxygen irrespective of population"
+#Move files to a new directory
 
-echo "Oxygen within population"
+mkdir all_pop
+mv all_pop_* all_pop/.
 
-echo "Normoxia vs. OC"
+#Next I will investigate differences between populations for each oxygen condition
 
+echo "Oxygen by population: OC"
 
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from NBH
+#in2: comma-separated bedGraphs from SC
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N5_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N4_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_OC-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_OC-S5_CG.sort.bedgraph \
+--groups N,S \
+--h1 OC-N5,OC-N1,OC-N2,OC-N4 \
+--h2 OC-S1,OC-S2,OC-S3,OC-S5 \
+--out /scratch/05-analysis/summarize/pop_diff \
+--cs ${CHROM_LENGTH}
 
+#Move files to a new directory
+
+mkdir pop_diff_OC
+mv pop_diff_*N_S* pop_diff_OC/.
+
+echo "Oxygen by population: Normoxia"
+
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from NBH
+#in2: comma-separated bedGraphs from SC
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-N4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_20-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-N1_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-S2_CG.sort.bedgraph \
+--groups N,S \
+--h1 20-N4,20-N2,20-N1 \
+--h2 20-S1,20-S3,20-S4,20-S2 \
+--out /scratch/05-analysis/summarize/pop_diff \
+--cs ${CHROM_LENGTH}
+
+#Move files to a new directory
+
+mkdir pop_diff_20
+mv pop_diff_*N_S* pop_diff_20/.
+
+echo "Oxygen by population: Hypoxia"
+
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from NBH
+#in2: comma-separated bedGraphs from SC
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-N3_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_5-S1_CG.sort.bedgraph \
+--groups N,S \
+--h1 5-N1,5-N2,5-N3 \
+--h2 5-S3,5-S4,5-S2,5-S1 \
+--out /scratch/05-analysis/summarize/pop_diff \
+--cs ${CHROM_LENGTH}
+
+#Move files to a new directory
+
+mkdir pop_diff_5
+mv pop_diff_*N_S* pop_diff_5/.
+
+echo "Oxygen within NB: Normoxia vs. Hypoxia"
+
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from NBH normoxia
+#in2: comma-separated bedGraphs from NBH hypoxia
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-N4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_20-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-N1_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-N3_CG.sort.bedgraph \
+--groups 20,5 \
+--h1 20-N4,20-N2,20-N1 \
+--h2 5-N1,5-N2,5-N3 \
+--out /scratch/05-analysis/summarize/N \
+--cs ${CHROM_LENGTH}
+
+#Move files to a new directory
+
+mkdir N_20_5
+mv N_*20_5* N_20_5/.
+
+echo "Oxygen within NB: Normoxia vs. OC"
+
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from NBH normoxia
+#in2: comma-separated bedGraphs from NBH OC
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-N4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_20-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-N1_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N5_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-N4_CG.sort.bedgraph \
+--groups 20,OC \
+--h1 20-N4,20-N2,20-N1 \
+--h2 OC-N5,OC-N1,OC-N2,OC-N4 \
+--out /scratch/05-analysis/summarize/N \
+--cs ${CHROM_LENGTH}
+
+#Move files to a new directory
+
+mkdir N_20_OC
+mv N_*20_OC* N_20_OC/.
+
+echo "Oxygen within SC: Normoxia vs. Hypoxia"
+
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from SC normoxia
+#in2: comma-separated bedGraphs from SC hypoxia
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-S2_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_5-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_5-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_5-S1_CG.sort.bedgraph \
+--groups 20,5 \
+--h1 20-S1,20-S3,20-S4,20-S2 \
+--h2 5-S3,5-S4,5-S2,5-S1 \
+--out /scratch/05-analysis/summarize/S \
+--cs ${CHROM_LENGTH}
+
+#Move files to a new directory
+
+mkdir S_20_5
+mv S_*20_5* S_20_5/.
+
+echo "Oxygen within SC: Normoxia vs. OC"
+
+#Run BAT_summarize
+#in1: comma-separated bedGraphs from SC normoxia
+#in2: comma-separated bedGraphs from SC OC
+#groups: comma-separated list of gorup identifiers
+#h1: comma-separated list of sample identifiers for group 1
+#h2: comma-seaprated list of sample identifiers for group 2
+#out: prefix for output files
+singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+/vortexfs1/home/naluru/bat_latest.sif \
+BAT_summarize \
+--in1 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L2_20-S4_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_20-S2_CG.sort.bedgraph \
+--in2 ${FILTERED}/190626_I114_FCH7TVNBBXY_L2_OC-S1_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-S2_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L3_OC-S3_CG.sort.bedgraph,${FILTERED}/190626_I114_FCH7TVNBBXY_L4_OC-S5_CG.sort.bedgraph \
+--groups 20,OC \
+--h1 20-S1,20-S3,20-S4,20-S2 \
+--h2 OC-S1,OC-S2,OC-S3,OC-S5 \
+--out /scratch/05-analysis/summarize/S \
+--cs ${CHROM_LENGTH}
+
+#Move files to a new directory
+
+mkdir S_20_OC
+mv S_*20_OC* S_20_OC/.
 
 echo "Done with summarize"
 
-echo "Overview Module"
+## TO DO: FIGURE OUT WHY OVERVIEW IS NOT WORKING
 
-echo "Population irrespective of oxygen"
+#echo "Overview Module"
+
+# echo "Population irrespective of oxygen"
 
 #Remove extra columns from output bedGraph
-awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14"\t"$15"\t"$16"\t"$17}' \
-${ALL_POP}_summary_N_S.bedgraph \
-> ${ALL_POP}_summary_N_S.bedgraph.fix
+#awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$14"\t"$15"\t"$16"\t"$17}' \
+#/scratch/05-analysis/summarize/all_pop_summary_N_S.bedgraph \
+#> /scratch/05-analysis/summarize/all_pop_summary_N_S.bedgraph.fix
 
 #Run BAT_overview
 #i: input summary bedGraph
 #o: output prefix
 #groups: comma-separated list of gorup identifiers
-singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
---bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
-/vortexfs1/home/naluru/bat_latest.sif \
-BAT_overview.R  \
--i ${ALL_POP}_summary_N_S.bedgraph.fix \
--o ${ALL_POP} \
---groups N,S
+#singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/05-analysis-envfile.txt \
+#--bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+#/vortexfs1/home/naluru/bat_latest.sif \
+#BAT_overview.R  \
+#-i /scratch/05-analysis/summarize/all_pop_summary_N_S.bedgraph.fix \
+#-o /scratch/05-analysis/summarize/all_pop \
+#--groups N,S
 
-BAT_overview.R  -i data/P10/${dataset}_summary_treatment_control.bedgraph -o data/P10/${dataset}_overview --groups treatment,control
-
-
-echo "Done with overview"
+#echo "Done with overview"
