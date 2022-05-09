0e90924b064e427baf9a9c8772446a87e9b638d3
Yaamini Venkataraman <yaaminiv@uw.edu>
2022-05-09 11:57:01 -0400

    new script for BAT_overview

---
+++ code/05-BAT-overview.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+echo "Population irrespective of oxygen"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/all_pop/all_pop_summary_N_S.bedgraph \
+-o ${OVERVIEW}/all_pop \
+--groups N,S
+
+echo "Oxygen by population: OC"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/OC_pop_diff/pop_diff_summary_N_S.bedgraph \
+-o ${OVERVIEW}/OC_pop_diff \
+--groups N,S
+
+echo "Oxygen by population: Normoxia"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/20_pop_diff/pop_diff_summary_N_S.bedgraph \
+-o ${OVERVIEW}/20_pop_diff \
+--groups N,S
+
+echo "Oxygen by population: Hypoxia"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/5_pop_diff/pop_diff_summary_N_S.bedgraph \
+-o ${OVERVIEW}/5_pop_diff \
+--groups N,S
+
+echo "Oxygen within NB: Normoxia vs. Hypoxia"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/20_5_N/N_summary_20_5.bedgraph \
+-o ${OVERVIEW}/20_5_N \
+--groups 20,5
+
+echo "Oxygen within NB: Normoxia vs. OC"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/20_OC_N/N_summary_20_OC.bedgraph \
+-o ${OVERVIEW}/20_OC_N \
+--groups 20,OC
+
+echo "Oxygen within SC: Normoxia vs. Hypoxia"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/20_5_S/S_summary_20_5.bedgraph \
+-o ${OVERVIEW}/20_5_S \
+--groups 20,5
+
+echo "Oxygen within SC: Normoxia vs. OC"
+
+#Run BAT_overview
+#-i: Input bedGraph from BAT_summarize
+#-o: Output prefix
+#--groups: comma-separated list of group identifiers
+
+BAT_overview.R  \
+-i ${SUMMARIZE}/20_OC_S/S_summary_20_OC.bedgraph \
+-o ${OVERVIEW}/20_OC_S \
+--groups 20,OC
