8d8a771b2406958214c69722618f3780892c1587
Yaamini Venkataraman <yaaminiv@uw.edu>
2022-09-13 13:44:51 -0400

    update scripts for calling from new genome alignments

--- code/04-calling.sh
+++ code/04-calling.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --partition=compute          								 				    			# Queue selection
-#SBATCH --job-name=yrv_calling          							 							  # Job name
-#SBATCH --mail-type=ALL              							   				     			# Mail events (BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=yaamini.venkataraman@whoi.edu    				    			# Where to send mail
-#SBATCH --nodes=1                                                     # One node
-#SBATCH --exclusive                                                   # All 36 procs on the one node
-#SBATCH --mem=100gb                                                   # Job memory request
-#SBATCH --output=yrv_calling%j.log  								   					  		# Standard output
-#SBATCH --chdir=/vortexfs1/scratch/yaamini.venkataraman/04-calling  	# Working directory for this script
+#SBATCH --partition=compute          								 				    		           	# Queue selection
+#SBATCH --job-name=yrv_calling          							 							            # Job name
+#SBATCH --mail-type=ALL              							   				     		           	# Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=yaamini.venkataraman@whoi.edu    				    		           	# Where to send mail
+#SBATCH --nodes=1                                                               # One node
+#SBATCH --exclusive                                                             # All 36 procs on the one node
+#SBATCH --mem=100gb                                                             # Job memory request
+#SBATCH --output=yrv_calling%j.log  								   					  	           	# Standard output
+#SBATCH --chdir=/vortexfs1/scratch/yaamini.venkataraman/04-calling/new-genome  	# Working directory for this script
 
 #Exit script if any command fails
 set -e
@@ -24,7 +24,7 @@ echo "Calling Module"
 
 #Run BAT_mapping script
 singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/04-calling-envfile.txt \
---bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+--bind /vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
 /vortexfs1/home/naluru/bat_latest.sif \
 /yaaminiv/04-BAT-calling.sh
 
@@ -34,7 +34,7 @@ echo "Filtering Module"
 
 #Run BAT_mapping_stat script
 singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/04-calling-envfile.txt \
---bind /vortexfs1/home/naluru/:/naluru,/vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
+--bind /vortexfs1/scratch/yaamini.venkataraman:/scratch,/vortexfs1/home/yaamini.venkataraman/:/yaaminiv \
 /vortexfs1/home/naluru/bat_latest.sif \
 /yaaminiv/04-BAT-calling-filtering.sh
 
