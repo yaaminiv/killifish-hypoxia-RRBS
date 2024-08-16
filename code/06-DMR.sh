3689a85f0c3d65a8baffb176316fd2a19dc78ca4
Yaamini Venkataraman <yaaminiv@uw.edu>
2024-08-16 15:42:11 -0400

    code for adding third column to RNA data

--- code/06-DMR.sh
+++ code/06-DMR.sh
@@ -15,6 +15,7 @@ module load singularity/3.7
 
 #Make scripts executable
 chmod +x /vortexfs1/home/yaamini.venkataraman/06-BAT-DMRcalling.sh
+chmod +x /vortexfs1/home/yaamini.venkataraman/06-BAT-correlating.sh
 
 echo "DMR Calling Module"
 
@@ -25,6 +26,21 @@ singularity exec --env-file /vortexfs1/home/yaamini.venkataraman/06-DMR-envfile.
 /yaaminiv/06-BAT-DMRcalling.sh
 
 echo "Done with DMR calling"
+
+echo "Reformat gene expression files"
+
+#Navigate to bedGraph directory
+#cd /vortexfs1/home/yaamini.venkataraman/killifish-hypoxia-RRBS/data/RNA-Seq-renamed
+
+#Sort bedGraphs
+#for f in *txt
+#do
+#awk -F "\t" '{ print $1, $1, $2 }' ${f} \
+#> $(basename ${f%.txt}).threeCol.txt
+#done
+#head *threeCol.txt
+
+echo "Done reformatting gene expression files"
 
 echo "DMR Correlating Module"
 
